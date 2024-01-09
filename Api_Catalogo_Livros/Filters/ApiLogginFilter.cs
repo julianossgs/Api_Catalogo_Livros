db793d42dd9f2d9284d3157081c45e9df6d674f6
Juliano Candido <julianosgs@yahoo.com.br>
2024-01-09 17:18:38 -0300

    Criação do Logging

---
+++ Api_Catalogo_Livros/Filters/ApiLogginFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api_Catalogo_Livros.Filters
+{
+    public class ApiLogginFilter
+    {
+        private readonly ILogger<ApiLogginFilter> _logger;
+        public ApiLogginFilter(ILogger<ApiLogginFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        //executa antes da Action
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation("### Executando -> OnActionExecuting");
+            _logger.LogInformation("###################################################");
+            _logger.LogInformation($"{DateTime.Now.ToLongTimeString()}");
+            _logger.LogInformation($"ModelState : {context.ModelState.IsValid}");
+            _logger.LogInformation("###################################################");
+        }
+        //executa depois da Action
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation("### Executando -> OnActionExecuted");
+            _logger.LogInformation("###################################################");
+            _logger.LogInformation($"{DateTime.Now.ToLongTimeString()}");
+            _logger.LogInformation("###################################################");
+        }
+    }
+}
---
+++ Api_Catalogo_Livros/Logging/CustomLoggerProviderConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Api_Catalogo_Livros.Logging
+{
+    public class CustomLoggerProviderConfiguration
+    {
+        public LogLevel LogLevel { get; set; } = LogLevel.Warning; //define o nível dos logs
+        public int EventId { get; set; } = 0;//define o id dos logs
+    }
+}
