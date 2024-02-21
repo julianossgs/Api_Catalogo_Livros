4634d701e0d416a6ffc3942de27dd7e1ba9aa94d
Juliano Candido <julianosgs@yahoo.com.br>
2024-02-21 16:38:39 -0300

    Finalizada Task_09_Documentação_WebAPI

--- Api_Catalogo_Livros/Controllers/AutoresController.cs
+++ Api_Catalogo_Livros/Controllers/AutoresController.cs
@@ -7,6 +7,8 @@ namespace Api_Catalogo_Livros.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
+    [ApiConventionType(typeof(DefaultApiConventions))]
     public class AutoresController : ControllerBase
     {
         private readonly AppDbContext _context;
@@ -17,7 +19,13 @@ namespace Api_Catalogo_Livros.Controllers
         }
 
         //Método que retorna Autores e seus livros
+        /// <summary>
+        /// Obter 1 lista de autores e seus livros
+        /// </summary>
+        /// <returns></returns>
         [HttpGet("livros")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<Autores>>> GetAutoresLivrosAsync()
         {
 
@@ -29,7 +37,14 @@ namespace Api_Catalogo_Livros.Controllers
 
 
         //método que retorna 1 lista 
+        /// <summary>
+        /// Retorna 1 lista
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<Autores>>> GetAsync()
         {
 
@@ -40,12 +55,19 @@ namespace Api_Catalogo_Livros.Controllers
             {
                 return NotFound("Lista não encontrada!");
             }
-            return autores;
+            return Ok(autores);
 
         }
 
         //método que retorna autor por id
+        /// <summary>
+        /// Obter 1 registro por ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id:int}", Name = "ObterAutor")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Autores>> GetAsync(int id)
         {
 
@@ -58,12 +80,22 @@ namespace Api_Catalogo_Livros.Controllers
             {
                 return NotFound("Autor não encontrado!");
             }
-            return autor;
+            return Ok(autor);
 
         }
 
         //add 
+        /// <summary>
+        /// Adiciona 1 novo registro
+        /// </summary>
+        /// <param name="autor"></param>
+        /// <returns></returns>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
         public ActionResult Add(Autores autor)
         {
 
@@ -80,7 +112,17 @@ namespace Api_Catalogo_Livros.Controllers
         }
 
         //update
+        /// <summary>
+        /// Atualiza 1 novo registro
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="autor"></param>
+        /// <returns></returns>
         [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
         public ActionResult Edit(int id, Autores autor)
         {
 
@@ -96,7 +138,14 @@ namespace Api_Catalogo_Livros.Controllers
         }
 
         //delete/excluir
+        /// <summary>
+        /// Exclui 1 novo registro
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult Delete(int id)
         {
 
--- Api_Catalogo_Livros/Controllers/EditorasController.cs
+++ Api_Catalogo_Livros/Controllers/EditorasController.cs
@@ -7,6 +7,9 @@ namespace Api_Catalogo_Livros.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+
     public class EditorasController : ControllerBase
     {
         private readonly AppDbContext _context;
@@ -17,7 +20,14 @@ namespace Api_Catalogo_Livros.Controllers
         }
 
         //método que retorna 1 lista 
+        /// <summary>
+        /// Retorna 1 Lista
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
         public async Task<ActionResult<IEnumerable<Editoras>>> GetAsync()
         {
 
@@ -28,13 +38,20 @@ namespace Api_Catalogo_Livros.Controllers
             {
                 return NotFound("Lista não encontrada!");
             }
-            return editoras;
+            return Ok(editoras);
 
         }
 
 
         //método que retorna editora por id
+        /// <summary>
+        /// Obter 1 registro por ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id:int}", Name = "ObterEditora")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Editoras>> GetAsync(int id)
         {
 
@@ -44,12 +61,22 @@ namespace Api_Catalogo_Livros.Controllers
             {
                 return NotFound("Editora não encontrada!");
             }
-            return editora;
+            return Ok(editora);
 
         }
 
         //add 
+        /// <summary>
+        /// Adiciona 1 novo registro
+        /// </summary>
+        /// <param name="editora"></param>
+        /// <returns></returns>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
         public ActionResult Add(Editoras editora)
         {
 
@@ -66,7 +93,17 @@ namespace Api_Catalogo_Livros.Controllers
         }
 
         //update
+        /// <summary>
+        /// Atualiza 1 novo registro
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="editora"></param>
+        /// <returns></returns>
         [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
         public ActionResult Edit(int id, Editoras editora)
         {
 
@@ -82,7 +119,14 @@ namespace Api_Catalogo_Livros.Controllers
         }
 
         //delete/excluir
+        /// <summary>
+        /// Exclui 1 registro
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult Delete(int id)
         {
 
